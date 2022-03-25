5f8004d3e35ed38ac60ba26e8db0050d0d9e9c93
vishal chaudhary <vishalchaudhary1975@gmail.com>
2022-03-25 22:50:34 +0530

    azure api added

--- src/main/java/com/ayush/finder/service/Distance_Find_Service.java
+++ src/main/java/com/ayush/finder/service/Distance_Find_Service.java
@@ -1,5 +1,8 @@
 package com.ayush.finder.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +13,42 @@ import org.springframework.web.client.RestTemplate;
 public class Distance_Find_Service {
 	RestTemplate restTemplate = new RestTemplate();
 	
-	public double getDistance(double start_longitude,double start_latitude,double dest_longitude,double dest_latitude) {
+//	public double getDistance(double start_longitude,double start_latitude,double dest_longitude,double dest_latitude) {
+//		
+//		
+//		
+////		RestTemplate restTemplate = new RestTemplate();
+////		String fooResourceUrl
+////		  = "https://apis.mapmyindia.com/advancedmaps/v1/33da8c0716b48b3f5f0e8dba54b11291/distance_matrix/driving/77.15605506826591,28.70978159989673;77.3756368423889,28.594492632792353";
+//		
+//		
+//		
+////String fooResourceUrl
+////		  = "https://apis.mapmyindia.com/advancedmaps/v1/33da8c0716b48b3f5f0e8dba54b11291/distance_matrix/driving/"+String.valueOf(start_longitude)+","+String.valueOf(start_latitude)+";"+String.valueOf(dest_longitude)+","+String.valueOf(dest_latitude);
+//		
+//		String fooResourceUrl
+//		  = "https://apis.mapmyindia.com/advancedmaps/v1/b8216fe7b9ba201f39a26f41de94b0d8/distance_matrix/driving/"+String.valueOf(start_longitude)+","+String.valueOf(start_latitude)+";"+String.valueOf(dest_longitude)+","+String.valueOf(dest_latitude);
+//	
+//		
+//		
+//		ResponseEntity<String> response
+//		  = restTemplate.getForEntity(fooResourceUrl , String.class);
+//		
+//		double distance=Double.MAX_VALUE;
+//		try {
+//			JSONObject jsonObject= new JSONObject(response.getBody());
+//			System.out.println(jsonObject.getJSONObject("results").getJSONArray("distances").getJSONArray(0));
+//			distance=(jsonObject.getJSONObject("results").getJSONArray("distances").getJSONArray(0).getDouble(1));
+//			
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return distance;
+//	}
+	
+	
+public double getDistance(double start_longitude,double start_latitude,double dest_longitude,double dest_latitude) {
 		
 		
 		
@@ -23,19 +61,27 @@ public class Distance_Find_Service {
 //String fooResourceUrl
 //		  = "https://apis.mapmyindia.com/advancedmaps/v1/33da8c0716b48b3f5f0e8dba54b11291/distance_matrix/driving/"+String.valueOf(start_longitude)+","+String.valueOf(start_latitude)+";"+String.valueOf(dest_longitude)+","+String.valueOf(dest_latitude);
 		
-		String fooResourceUrl
-		  = "https://apis.mapmyindia.com/advancedmaps/v1/b8216fe7b9ba201f39a26f41de94b0d8/distance_matrix/driving/"+String.valueOf(start_longitude)+","+String.valueOf(start_latitude)+";"+String.valueOf(dest_longitude)+","+String.valueOf(dest_latitude);
+//		String fooResourceUrl="https://atlas.microsoft.com/route/directions/json";
 	
+	String fooResourceUrl="https://atlas.microsoft.com/route/directions/json?api-version={api-version}&query={query}&travelMode={travelMode}&subscription-key={subscription-key}";
+	
+		Map<String, String> vars = new HashMap<>();
 		
+		String querry= start_latitude+","+start_longitude+":"+dest_latitude+","+dest_longitude;
+		
+		vars.put("api-version", "1.0");
+		vars.put("query", querry);
+		vars.put("travelMode","car");
+		vars.put("subscription-key", "Qr06pfw8OqZm9fnjLAKJbMKpmolImw93RBb0F7k7Jrw");
 		
 		ResponseEntity<String> response
-		  = restTemplate.getForEntity(fooResourceUrl , String.class);
+		  = restTemplate.getForEntity(fooResourceUrl , String.class,vars);
 		
 		double distance=Double.MAX_VALUE;
 		try {
 			JSONObject jsonObject= new JSONObject(response.getBody());
-			System.out.println(jsonObject.getJSONObject("results").getJSONArray("distances").getJSONArray(0));
-			distance=(jsonObject.getJSONObject("results").getJSONArray("distances").getJSONArray(0).getDouble(1));
+			System.out.println(jsonObject.getJSONArray("routes").getJSONObject(0).getJSONObject("summary").getDouble("lengthInMeters"));
+			distance=(jsonObject.getJSONArray("routes").getJSONObject(0).getJSONObject("summary").getDouble("lengthInMeters"));
 			
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
